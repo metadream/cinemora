d6d0aeac725ef96f8d201c603c8d35234d141299
metadream <app@zerg.cc>
2024-04-29 18:19:32 +0800

    index

--- src/main/java/com/arraywork/puffin/controller/PortalController.java
+++ src/main/java/com/arraywork/puffin/controller/PortalController.java
@@ -4,25 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.arraywork.puffin.service.PreferenceService;
+import com.arraywork.puffin.entity.Metadata;
+import com.arraywork.puffin.service.MetadataService;
 
 import jakarta.annotation.Resource;
 
 /**
- * 首页控制器
+ * 门户控制器
  * @author AiChen
  * @copyright ArrayWork Inc.
  * @since 2024/04/22
  */
 @Controller
-public class IndexController {
+public class PortalController {
 
     @Resource
-    private PreferenceService prefsService;
+    private MetadataService metadataService;
 
     // 首页
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, String page, Metadata condition) {
+        model.addAttribute("pagination", metadataService.getMetadatas(page, condition));
         return "index";
     }
 
