6be23704ef024ced532658b1385bd02a3be632b3
metadream <app@zerg.cc>
2024-04-30 22:01:15 +0800

    Create LibraryController.java

---
+++ src/main/java/com/arraywork/puffin/controller/LibraryController.java
@@ -0,0 +1,33 @@
+package com.arraywork.puffin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.arraywork.springforce.SseChannel;
+
+import jakarta.annotation.Resource;
+
+/**
+ * 媒体库控制器
+ * @author AiChen
+ * @copyright ArrayWork Inc.
+ * @since 2024/04/30
+ */
+@Controller
+@RequestMapping("/~")
+public class LibraryController {
+
+    @Resource
+    private SseChannel channel;
+
+    // SSE获取扫描状态
+    @GetMapping("/status")
+    @ResponseBody
+    public SseEmitter status() {
+        return channel.subscribe();
+    }
+
+}
