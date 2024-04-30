9bb96a101ee1258e5f21049c2c32bdfb213ae646
metadream <app@zerg.cc>
2024-04-30 22:50:58 +0800

    Update LibraryListener.java

--- src/main/java/com/arraywork/puffin/service/LibraryListener.java
+++ src/main/java/com/arraywork/puffin/service/LibraryListener.java
@@ -1,10 +1,16 @@
 package com.arraywork.puffin.service;
 
 import java.io.File;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Component;
 
-import com.arraywork.puffin.entity.ScanStatus;
+import com.arraywork.puffin.entity.Metadata;
+import com.arraywork.puffin.entity.Scanning;
+import com.arraywork.puffin.enums.ScanEvent;
+import com.arraywork.puffin.enums.ScanState;
+import com.arraywork.springforce.SseChannel;
 import com.arraywork.springforce.filewatch.FileSystemListener;
 import com.arraywork.springforce.util.Times;
 
@@ -19,51 +25,66 @@ import jakarta.annotation.Resource;
 @Component
 public class LibraryListener implements FileSystemListener {
 
-    private ScanStatus scanStatus = new ScanStatus();
+    private static List<Scanning> scanLogs = new CopyOnWriteArrayList<>();
+
     @Resource
     private MetadataService metadataService;
+    @Resource
+    private SseChannel channel;
 
+    // 监听启动回调方法
     @Override
     public void onStarted(File file, int count, int total) {
-        scanStatus.path = file.getPath();
-        scanStatus.count = count;
-        scanStatus.total = total;
-        scanStatus.event = "扫描";
-        scanStatus.result = null;
-        scanStatus.error = null;
+        Times.delay(300);
+        Scanning scanning = new Scanning(ScanEvent.SCAN);
+        scanning.count = count;
+        scanning.total = total;
+        scanning.path = file.getPath();
 
         try {
-            Times.delay(100);
-            metadataService.create(file);
-            scanStatus.result = "成功";
-            scanStatus.success++;
+            Metadata metadata = metadataService.build(file);
+            if (metadata != null) {
+                scanning.state = ScanState.SUCCESS;
+            } else {
+                scanning.state = ScanState.SKIPPED;
+            }
         } catch (Exception e) {
-            scanStatus.result = "失败";
-            scanStatus.error = e.getMessage();
-            scanStatus.failed++;
+            scanning.state = ScanState.FAILED;
+            scanning.message = e.getMessage();
+        } finally {
+            scanLogs.add(scanning);
+            channel.broadcast(scanning);
+
+            if (count == total) {
+                scanning = new Scanning(ScanEvent.SCAN);
+                scanning.total = total;
+                scanning.state = ScanState.FINISHED;
+                scanning.message = "本次扫描共发现" + scanning.total + "个文件。成功" + 0 + "个，跳过" + 0 + "个，失败" + 0 + "个。";
+                scanLogs.add(scanning);
+                channel.broadcast(scanning);
+            }
         }
-        System.out.println(scanStatus);
     }
 
+    // 新增文件回调方法
     @Override
-    public void onAdded(File file) {
-        System.out.print("Added: ");
-        System.out.println(file);
-
-        // CommonUtils.delay(500);
-        // metadataService.create(file);
+    public void onAdded(File file, int count, int total) {
+        Times.delay(300);
+        Metadata metadata = metadataService.build(file);
     }
 
+    // 修改文件回调方法
     @Override
-    public void onModified(File file) {
-        System.out.print("Modified: ");
-        System.out.println(file);
+    public void onModified(File file, int count, int total) {
+        Times.delay(300);
+        Metadata metadata = metadataService.build(file);
     }
 
+    // 删除文件回调方法
     @Override
-    public void onDeleted(File file) {
-        System.out.print("Deleted: ");
-        System.out.println(file);
+    public void onDeleted(File file, int count, int total) {
+        Times.delay(300);
+        Metadata metadata = metadataService.delete(file);
     }
 
 }
