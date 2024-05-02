3457b1e23f5985e67eb67f9032bb07204d0842b3
metadream <app@zerg.cc>
2024-05-02 21:23:11 +0800

    Create TagCloudService.java

---
+++ src/main/java/com/arraywork/puffin/service/TagCloudService.java
@@ -0,0 +1,72 @@
+package com.arraywork.puffin.service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.arraywork.puffin.entity.Metadata;
+import com.arraywork.puffin.entity.TagCloud;
+import com.arraywork.puffin.repo.MetadataRepo;
+import com.arraywork.springforce.util.ExpiringCache;
+
+import jakarta.annotation.Resource;
+
+/**
+ * 标签云服务
+ * @author AiChen
+ * @copyright ArrayWork Inc.
+ * @since 2024/05/02
+ */
+@Service
+public class TagCloudService {
+
+    private static final String TAG_CLOUD_KEY = "TAG_CLOUD_KEY";
+
+    @Resource
+    private ExpiringCache<String, TagCloud> cache;
+    @Resource
+    private MetadataRepo metadataRepo;
+
+    // 获取标签云
+    public TagCloud getTagCloud() {
+        TagCloud tagCloud = cache.get(TAG_CLOUD_KEY);
+        if (tagCloud != null) return tagCloud;
+
+        tagCloud = new TagCloud();
+        Set<String> allProducers = new HashSet<>();
+        Set<String> allDirectors = new HashSet<>();
+        Set<String> allStarring = new HashSet<>();
+        Set<String> allSeries = new HashSet<>();
+        Set<String> allGenres = new HashSet<>();
+        Set<String> allTags = new HashSet<>();
+        tagCloud.setProducers(allProducers);
+        tagCloud.setDirectors(allDirectors);
+        tagCloud.setStarring(allStarring);
+        tagCloud.setSeries(allSeries);
+        tagCloud.setGenres(allGenres);
+        tagCloud.setTags(allTags);
+
+        List<Metadata> results = metadataRepo.findAll();
+        for (Metadata metadata : results) {
+            String[] producers = metadata.getProducers();
+            String[] directors = metadata.getDirectors();
+            String[] starring = metadata.getStarring();
+            String[] series = metadata.getSeries();
+            String[] genres = metadata.getGenres();
+            String[] tags = metadata.getTags();
+
+            if (producers != null) allProducers.addAll(Arrays.asList(producers));
+            if (directors != null) allDirectors.addAll(Arrays.asList(directors));
+            if (starring != null) allStarring.addAll(Arrays.asList(starring));
+            if (series != null) allSeries.addAll(Arrays.asList(series));
+            if (genres != null) allGenres.addAll(Arrays.asList(genres));
+            if (tags != null) allTags.addAll(Arrays.asList(tags));
+        }
+        cache.put(TAG_CLOUD_KEY, tagCloud, 10);
+        return tagCloud;
+    }
+
+}
